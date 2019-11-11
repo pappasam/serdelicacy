2b2b33c9e2020b3b0d4a90c4b09d6ac817b37ca4
Sam Roeca <samuel.roeca@gmail.com>
2019-11-10 20:39:18 -0500

    Fix error handling

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ It has no external dependencies. Python 3.8+.
 
 ## Notes
 
-* Recursive types aren't currently supported by mypy. I intend to wait until they are before releasing this library.
+* Recursive types aren't currently supported by mypy. I have them anyway because recursive types will allegedly be supported soon. That said, the type checker isn't super happy with me at the moment.
 * Edge cases haven't been considered. This library is a learning project, for now.
 * Inspired by [undictify](https://github.com/Dobiasd/undictify), but special-cased to dataclasses and more-focused on serde instead of general function signature overrides.
 
--- serdataclasses/deserialize.py
+++ serdataclasses/deserialize.py
@@ -2,102 +2,152 @@
 
 from dataclasses import is_dataclass, InitVar
 from typing import get_type_hints
-from typing import Type, Any, Union
+from typing import Type, Any, Union, List
 
-from .typedefs import JsonType, T
+from .typedefs import JsonValuesType, T
 
 
 class DeserializeError(Exception):
-    """Exception for when deserialization fails"""
+    """Exception for deserialization failure
+
+    Deserializing arbitrarily-nested JSON often results in opaque
+    deserialization errors. This Exception class provides a clear, consistent
+    debugging message. Example:
+
+        serdataclasses.deserialize.DeserializeError: Expected '<class 'int'>'
+        but received '<class 'str'>' for value '2'.
+
+        Error location: <class 'test_all.MyDataClass'> >>>
+        typing.List[test_all.Another] >>> <class 'test_all.Another'> >>>
+        typing.List[int] >>> <class 'int'>
+    """
+
+    def __init__(
+        self, type_expected: Type, value_received: object, depth: List[Type]
+    ):
+        message = (
+            f"Expected '{type_expected}' but received '{type(value_received)}'"
+            f" for value '{value_received}'."
+        )
+        depth_str = " >>> ".join([str(item) for item in depth])
+        super().__init__(f"{message}\nError location: {depth_str}")
 
 
-def deserialize(obj: JsonType, constructor: Type[T]) -> T:
+def deserialize(obj: JsonValuesType, constructor: Type[T]) -> T:
+    """Deserialize an object into its constructor
+
+    :param obj: the 'serialized' object that we want to deserialize
+    :param constructor: the type into which we want serialize 'obj'
+    """
+    return _deserialize(obj, constructor, [])
+
+
+def _deserialize(
+    obj: JsonValuesType, constructor: Type[T], depth: List[Type]
+) -> T:
     """Deserialize the type
+
+    :param depth: keeps track of recursive position. Supremely helpful for
+    error messages, since it shows the user exactly where the parsing fails.
 
     NOTE: we use typing.get_type_hints because it correctly handles
     ForwardRefs, translating string references into their correct type in
     strange and mysterious ways.
     """
-    if _is_dataclass(obj, constructor):
+    new_depth = depth + [constructor]
+    if _is_dataclass(obj, constructor, new_depth):
         return constructor(
             **{
-                name: deserialize(obj.get(name), _type)
+                name: _deserialize(obj.get(name), _type, new_depth)
                 for name, _type in get_type_hints(constructor).items()
             }
         )
-    if _is_list(obj, constructor):
+    if _is_list(obj, constructor, new_depth):
         return [
-            deserialize(value, getattr(constructor, "__args__", (object,))[0])
+            _deserialize(
+                value,
+                getattr(constructor, "__args__", (object,))[0],
+                new_depth,
+            )
             for value in obj
         ]
     if isinstance(constructor, InitVar):
-        return deserialize(obj, constructor.type)
+        return _deserialize(obj, constructor.type, new_depth)
     if (
-        _is_nonetype(obj, constructor)
-        or _is_any(obj, constructor)
-        or _is_primitive(obj, constructor)
+        _is_nonetype(obj, constructor, new_depth)
+        or _is_any(obj, constructor, new_depth)
+        or _is_primitive(obj, constructor, new_depth)
     ):
         return obj
-    if _is_union(obj, constructor):
+    if _is_union(obj, constructor, new_depth):
         _union_errors = []
         for argument in constructor.__args__:
             try:
-                return deserialize(obj, argument)
+                return _deserialize(obj, argument, new_depth)
             except DeserializeError as error:
                 _union_errors.append(str(error))
-        raise DeserializeError(
-            f"Failed to deserialize '{obj}' with '{constructor}' "
-            f"for all provided types: {' && '.join(_union_errors)}"
-        )
+        raise DeserializeError(constructor, obj, new_depth)
 
-    raise DeserializeError(f"Cannot deserialize {obj} with {constructor}")
+    raise DeserializeError(constructor, obj, new_depth)
 
 
-def _is_dataclass(obj: JsonType, constructor: Type) -> bool:
+def _is_dataclass(
+    obj: JsonValuesType, constructor: Type, depth: List[Type]
+) -> bool:
     """Check if the type is a dataclasses"""
     if is_dataclass(constructor):
         if not isinstance(obj, dict):
-            raise DeserializeError(f"Expected Dict, received {type(obj)}")
+            raise DeserializeError(dict, obj, depth)
         return True
     return False
 
 
-def _is_list(obj: JsonType, constructor: Type) -> bool:
+def _is_list(
+    obj: JsonValuesType, constructor: Type, depth: List[Type]
+) -> bool:
     """Check if the type is a list"""
     if constructor == list or getattr(constructor, "__origin__", None) == list:
         if not isinstance(obj, list):
-            raise DeserializeError(f"Expected list, received {type(obj)}")
+            raise DeserializeError(list, obj, depth)
         return True
     return False
 
 
-def _is_nonetype(obj: JsonType, constructor: Type) -> bool:
+def _is_nonetype(
+    obj: JsonValuesType, constructor: Type, depth: List[Type]
+) -> bool:
     """Check if type is NoneType, or None"""
-    if constructor is None or constructor == type(None):
+    if constructor == type(None):
         if not obj is None:
-            raise DeserializeError(f"Expected NoneType, received {type(obj)}")
+            raise DeserializeError(type(None), obj, depth)
         return True
     return False
 
 
 def _is_any(
-    obj: JsonType, constructor: Type  # pylint: disable=unused-argument
+    obj: JsonValuesType,  # pylint: disable=unused-argument
+    constructor: Type,
+    depth: List[Type],  # pylint: disable=unused-argument
 ) -> bool:
     """Check if type is Any"""
     return constructor in (Any, object, InitVar)
 
 
-def _is_primitive(obj: JsonType, constructor: Type) -> bool:
+def _is_primitive(
+    obj: JsonValuesType, constructor: Type, depth: List[Type]
+) -> bool:
     """Check if a type is a primitive type"""
     if constructor in (str, int, float, bool):
         if not isinstance(obj, constructor):
-            raise DeserializeError(f"Expected {constructor}, got {type(obj)}")
+            raise DeserializeError(constructor, obj, depth)
         return True
     return False
 
 
 def _is_union(
-    obj: JsonType, constructor: Type  # pylint: disable=unused-argument
+    obj: JsonValuesType,  # pylint: disable=unused-argument
+    constructor: Type,
+    depth: List[Type],  # pylint: disable=unused-argument
 ) -> bool:
     """check whether a type is Union
 
