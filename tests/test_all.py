6f4d1ce244d53cb0a2d09f41cbd9477c7fd205bd
Sam Roeca <samuel.roeca@gmail.com>
2019-11-16 18:07:57 -0500

    Add initial tests using hypothesis

--- tests/test_all.py
+++ tests/test_all.py
@@ -6,55 +6,49 @@ from typing import List, Union, Optional
 from serdataclasses.deserialize import deserialize
 from serdataclasses.serialize import serialize
 
+from hypothesis import given, assume, settings, strategies as st
+
+# pylint: disable=missing-class-docstring,missing-function-docstring,invalid-name
+
 
 @dataclass
-class Another:
-    y: List[int]
+class Small:
+    my_list_int: List[int]
 
 
 @dataclass
-class MyDataClass:
-    x: int
-    y: str
-    z: float
-    l: List["Another"]
-    q: Optional[str]
-    p: List[Union[Another, str]]
-    b: List
+class Big:
+    my_int: int
+    my_str: str
+    my_float: float
+    my_list_small: List["Small"]
+    my_optional_str: Optional[str]
+    my_list_of_small_or_str: List[Union[Small, str]]
+    my_list: List
 
 
-value = {
-    "x": 12,
-    "y": "hello",
-    "z": 12.41,
-    "l": [{"y": [1, 2, 3]}, {"y": [4, 5, 6]}],
-    "p": ["hello", "world", {"y": [1, 2, 3]}, {"y": [4, 5, 6]}],
-    "b": [tuple(), tuple(), tuple()]
-}
-
-deserialized = deserialize(value, MyDataClass)
-
-serialized = serialize(deserialized)
-
-expected_deserialized = MyDataClass(
-    x=12,
-    y="hello",
-    z=12.41,
-    l=[Another(y=[1, 2, 3]), Another(y=[4, 5, 6])],
-    q=None,
-    p=["hello", "world", Another(y=[1, 2, 3]), Another(y=[4, 5, 6])],
-    b=[tuple(), tuple(), tuple()],
+SmallData = st.fixed_dictionaries({"my_list_int": st.lists(st.integers())})
+BigData = st.fixed_dictionaries(
+    {
+        "my_int": st.integers(),
+        "my_str": st.text(),
+        "my_float": st.floats(),
+        "my_list_small": st.lists(SmallData),
+        "my_optional_str": st.one_of(st.none(), st.text()),
+        "my_list_of_small_or_str": st.lists(st.one_of(SmallData, st.text())),
+        "my_list": st.lists(st.text()),
+    }
 )
 
-expected_serialized = {
-    **value,
-    "q": None,
-}
 
-
-def test_deserialize():
-    assert deserialized == expected_deserialized
-
-
-def test_serialize():
-    assert serialized == expected_serialized
+@given(BigData)
+def test_serde_big_data(big_data: dict):
+    deserialized = deserialize(big_data, Big)
+    assert deserialized.my_int == big_data["my_int"]
+    assert deserialized.my_list == big_data["my_list"]
+    assert deserialized.my_list_of_small_or_str == [
+        Small(**value) if isinstance(value, dict) else value
+        for value in big_data["my_list_of_small_or_str"]
+    ]
+    serialized = serialize(deserialized)
+    assert big_data == serialized
