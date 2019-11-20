2968c5303a37637cc76b6467dbb0258b53ff1df1
Sam Roeca <samuel.roeca@gmail.com>
2019-11-19 23:04:02 -0500

    Bool functions only check for constructor type

--- serdataclasses/deserialize.py
+++ serdataclasses/deserialize.py
@@ -64,29 +64,35 @@ def _deserialize(
     strange and mysterious ways.
     """
     new_depth = depth + [constructor]
-    if _is_dataclass(obj, constructor, new_depth) or _is_namedtuple(
-        obj, constructor, new_depth
-    ):
+    if _is_dataclass(constructor) or _is_namedtuple(constructor):
+        if not isinstance(obj, dict):
+            raise DeserializeError(dict, obj, depth)
         return constructor(
             **{
                 name: _deserialize(obj.get(name), _type, new_depth)
                 for name, _type in get_type_hints(constructor).items()
             }
         )
-    if _is_list(obj, constructor, new_depth):
+    if _is_list(constructor):
+        if not isinstance(obj, list):
+            raise DeserializeError(list, obj, depth)
         return [
             _deserialize(value, get_args(constructor)[0], new_depth)
             for value in obj
         ]
-    if isinstance(constructor, InitVar):
+    if _is_initvar(constructor):
         return _deserialize(obj, constructor.type, new_depth)
-    if (
-        _is_nonetype(obj, constructor, new_depth)
-        or _is_any(obj, constructor, new_depth)
-        or _is_primitive(obj, constructor, new_depth)
-    ):
+    if _is_nonetype(constructor):
+        if not obj is None:
+            raise DeserializeError(type(None), obj, depth)
         return obj
-    if _is_union(obj, constructor, new_depth):
+    if _is_primitive(constructor):
+        if not isinstance(obj, constructor):
+            raise DeserializeError(constructor, obj, depth)
+        return obj
+    if _is_any(constructor):
+        return obj
+    if _is_union(constructor):
         _union_errors = []
         for argument in get_args(constructor):
             try:
@@ -94,7 +100,7 @@ def _deserialize(
             except DeserializeError as error:
                 _union_errors.append(str(error))
         raise DeserializeError(constructor, obj, new_depth)
-    if _is_typevar(obj, constructor, new_depth):
+    if _is_typevar(constructor):
         return _deserialize(
             obj,
             (
@@ -110,64 +116,37 @@ def _deserialize(
     )
 
 
-def _is_dataclass(
-    obj: JsonValuesType, constructor: Type, depth: List[Type]
-) -> bool:
+def _is_dataclass(constructor: Type) -> bool:
     """Check if the type is a dataclasses"""
-    if is_dataclass(constructor):
-        if not isinstance(obj, dict):
-            raise DeserializeError(dict, obj, depth)
-        return True
-    return False
+    return is_dataclass(constructor)
 
 
-def _is_list(
-    obj: JsonValuesType, constructor: Type, depth: List[Type]
-) -> bool:
+def _is_initvar(constructor: Type) -> bool:
+    """Check if the type is an InitVar"""
+    return isinstance(constructor, InitVar)
+
+
+def _is_list(constructor: Type) -> bool:
     """Check if the type is a list"""
-    if constructor == list or get_origin(constructor) == list:
-        if not isinstance(obj, list):
-            raise DeserializeError(list, obj, depth)
-        return True
-    return False
+    return constructor == list or get_origin(constructor) == list
 
 
-def _is_nonetype(
-    obj: JsonValuesType, constructor: Type, depth: List[Type]
-) -> bool:
+def _is_nonetype(constructor: Type) -> bool:
     """Check if type is NoneType, or None"""
-    if constructor == type(None):
-        if not obj is None:
-            raise DeserializeError(type(None), obj, depth)
-        return True
-    return False
+    return constructor == type(None)
 
 
-def _is_any(
-    obj: JsonValuesType,  # pylint: disable=unused-argument
-    constructor: Type,
-    depth: List[Type],  # pylint: disable=unused-argument
-) -> bool:
+def _is_any(constructor: Type) -> bool:
     """Check if type is Any"""
     return constructor in (Any, object, InitVar)
 
 
-def _is_primitive(
-    obj: JsonValuesType, constructor: Type, depth: List[Type]
-) -> bool:
+def _is_primitive(constructor: Type) -> bool:
     """Check if a type is a primitive type"""
-    if constructor in (str, int, float, bool):
-        if not isinstance(obj, constructor):
-            raise DeserializeError(constructor, obj, depth)
-        return True
-    return False
+    return constructor in (str, int, float, bool)
 
 
-def _is_union(
-    obj: JsonValuesType,  # pylint: disable=unused-argument
-    constructor: Type,
-    depth: List[Type],  # pylint: disable=unused-argument
-) -> bool:
+def _is_union(constructor: Type) -> bool:
     """check whether a type is Union
 
     Note: Optional[str] is an alias for Union[str, NoneType]
@@ -175,15 +154,11 @@ def _is_union(
     return get_origin(constructor) == Union
 
 
-def _is_typevar(
-    obj: JsonValuesType, constructor: Type, depth: List[Type]
-) -> bool:
+def _is_typevar(constructor: Type) -> bool:
     """Check if a type is a TypeVar"""
     return isinstance(constructor, TypeVar)
 
 
-def _is_namedtuple(
-    obj: JsonValuesType, constructor: Type, depth: List[Type]
-) -> bool:
+def _is_namedtuple(constructor: Type) -> bool:
     """Check if a type is a NamedTuple"""
     return isinstance(constructor, NamedTupleType)
